43cb46d426877658b3a3cbaf18f564c3c6b23511
AbdelrahmanElkhradely <65429772+AbdelrahmanElkhradely@users.noreply.github.com>
2020-12-21 16:35:32 +0200

    A

--- Human.py
+++ Human.py
@@ -4,22 +4,27 @@ from Country import *
 import ctypes
 
 
-class Human:
+class Human(Agent):
 
     def canAttack(self,countryAttackFrom,countryAttackTo,amountOfTroops):
         if countryAttackFrom.owner== self and \
                 countryAttackTo in countryAttackFrom.neighbors and \
                 amountOfTroops-1 <= countryAttackFrom.numOfTroops and \
                 amountOfTroops-1 > countryAttackTo.numOfTroops :
-            self.attacking(self,countryAttackFrom,countryAttackTo,amountOfTroops)
+            return True
         else:
-            ctypes.windll.user32.MessageBoxW(0, "invalid attack", "ALERT", 1)
+            return False
             
 
-    def attacking(self,countryAttackFrom,countryAttackTo,amountOfTroops):
-         countryAttackFrom.numOfTroops=countryAttackFrom.numOfTroops-amountOfTroops
-         countryAttackTo=amountOfTroops
-         countryAttackTo.owner = self
+    def attack(self,countryAttackFrom,countryAttackTo,amountOfTroops):
+         flag=self.canAttack(self,countryAttackFrom,countryAttackTo,amountOfTroops)
+         if flag:
+            countryAttackFrom.numOfTroops=countryAttackFrom.numOfTroops-amountOfTroops
+            countryAttackTo=amountOfTroops
+            countryAttackTo.owner = self
+         else:
+            ctypes.windll.user32.MessageBoxW(0, "invalid attack", "ALERT", 1)
+     
          
 
 
---
+++ US_STATE.py
@@ -0,0 +1,2 @@
+class US_STATE:
+    
